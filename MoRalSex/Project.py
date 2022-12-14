368f8ee2ca071b912fa36b3b3a959ac6ae00f4dd
ivanmash <63999058+ivanmash@users.noreply.github.com>
2022-12-14 17:26:31 +0300

    Update Project.py

--- MoRalSex/Project.py
+++ MoRalSex/Project.py
@@ -29,45 +29,45 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('oral_sex_eng_translation.csv')
 
 
-# ## The Dataset itself
+st.header("The Dataset itself")
 
 # In[4]:
 
 
-data
+st.write(data)
 
 
 # In[5]:
 
 
-print(data["Age"].mean())
+st.write(data["Age"].mean())
 
 
 # In[6]:
 
 
-print(data["Age"].std())
+st.write(data["Age"].std())
 
 
 # In[7]:
 
 
-print(data["Age"].median())
+st.write(data["Age"].median())
 
 
-# ## Data description
+st.header("Data description")
 
 # In[8]:
 
 
-data["Age"].describe()
+st.write(data["Age"].describe())
 
 
 # In[9]:
 
 
 categories = [col for col in data.columns if data[col].dtype=='O']
-categories
+st.write(categories)
 
 
 # In[10]:
@@ -77,55 +77,55 @@ uqique_categories_values={}
 for colm in categories:
     uqique_categories_values[colm] = data[colm].unique()
 
-uqique_categories_values
+st.write(uqique_categories_values)
 
 
-# ## Pretty clean, don’t you think?
+st.header("Pretty clean, don’t you think?")
 
 # In[11]:
 
 
-data.isnull().values.any()
+st.write(data.isnull().values.any())
 
 
 # In[12]:
 
 
-data.isnull().sum().sum()
+st.write(data.isnull().sum().sum())
 
 
-# ## Let's add some columns!
+st.header("Let's add some columns!")
 
 # In[13]:
 
 
 data["Difference in age from max"] = max(data["Age"]) - data["Age"]
-data
+st.write(data)
 
 
 # In[14]:
 
 
-data["Difference in age from max"].describe()
+st.write(data["Difference in age from max"].describe())
 
 
 # In[15]:
 
 
-data["Difference in age from max"].median()
+st.write(data["Difference in age from max"].median())
 
 
-# ## Plots
+st.header("Plots")
 
 # In[16]:
 
-
+fig = plt.figure(figsize=(10, 4))
 sns.histplot(data['Age'], kde=True, bins=40)
 plt.ylabel("Number of people")
-plt.show()
+st.pyplot(fig)
 
 
-# Age is not normally distributed.
+st.subheader("Age is not normally distributed.")
 
 # In[17]:
 
@@ -146,10 +146,10 @@ colours = ['red', 'blue', 'green', 'yellow','purple', 'orange']
 fig = px.pie(data, values = [df12, df23, df34, df45, df56, df6], names = ["18-20", "20-30", "30-40", "40-50", "50-60", "60+"], title = "Age in percentage", color = colours)
 fig.update_traces(textposition='inside')
 fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
-fig.show()
+st.pyplot(fig)
 
 
-# The biggest number of people, who took the survey, were from age 40 to 50 y.o.
+st.subheader("The biggest number of people, who took the survey, were from age 40 to 50 y.o.")
 
 # In[19]:
 
@@ -160,7 +160,7 @@ def OverviewPlots(data, nGraphShown, nGraphPerRow):
     nRow, nCol = data.shape
     columnNames = list(data)
     nGraphRow = (nCol + nGraphPerRow - 1) // nGraphPerRow
-    plt.figure(figsize = (4 * nGraphPerRow, 6 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    fig = plt.figure(figsize = (4 * nGraphPerRow, 6 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
     for i in range(min(nCol, nGraphShown)):
         plt.subplot(nGraphRow, nGraphPerRow, i + 1)
         columndata = data.iloc[:, i]
@@ -170,7 +170,7 @@ def OverviewPlots(data, nGraphShown, nGraphPerRow):
         plt.xticks(rotation = 90)
         plt.title(f'{columnNames[i]} (column {i})')
     plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
-    plt.show()
+    st.pyplot(fig)
 
 
 # In[20]:
@@ -179,21 +179,21 @@ def OverviewPlots(data, nGraphShown, nGraphPerRow):
 OverviewPlots(data, 8, 4)
 
 
-# General information about given dataset.
+st.subheader("General information about given dataset.")
 
 # In[21]:
 
 
-plt.figure(figsize=(12,6))
+fig = plt.figure(figsize=(12,6))
 sns.swarmplot(y=data['Age'],x=data['Sex'],hue=data['Do you engage in oral sex?'])
-plt.show()
+st.pyplot(fig)
 
 
-# This graph shows us answers to the survey and with their age and sex. As we see in it, in most cases both sides were engaged in oral sex with mutual agreement. Although, as we can see with men, the other significant part initiated it on their own decision, whereas on the women side it is clear, that mutual agreement is in a leading position and many of them didn't have vaginal sex nor anal sex. Moreover, as the age is getting bigger it becomes more likely that people don't remember being engaged in oral sex or didn't participate in it at all.
+st.subheader("This graph shows us answers to the survey and with their age and sex. As we see in it, in most cases both sides were engaged in oral sex with mutual agreement. Although, as we can see with men, the other significant part initiated it on their own decision, whereas on the women side it is clear, that mutual agreement is in a leading position and many of them didn't have vaginal sex nor anal sex. Moreover, as the age is getting bigger it becomes more likely that people don't remember being engaged in oral sex or didn't participate in it at all.")
 
-# ## Hypothesis
+st.header("Hypothesis")
 
-# In general, people mostly do anal sex and for them, it is not important, whether they have eye contact or not (especially for men). Moreover, my assumption is that sex is popular during University life and most people do it in the age form 20-40 years. And women are the most interested part in holding such an event.
+st.header("In general, people mostly do anal sex and for them, it is not important, whether they have eye contact or not (especially for men). Moreover, my assumption is that sex is popular during University life and most people do it in the age form 20-40 years. And women are the most interested part in holding such an event.")
 
 # In[22]:
 
@@ -201,10 +201,10 @@ plt.show()
 fig = px.pie(data, values = data["Sex"].value_counts(), names = data["Sex"].unique(), title = "Sex comparison")
 fig.update_traces(textposition='inside')
 fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
-fig.show()
+st.pyplot(fig)
 
 
-# As we can see from the pie-chart, the most popular sex in this survey is men.
+st.subheader("As we can see from the pie-chart, the most popular sex in this survey is men.")
 
 # In[23]:
 
@@ -216,11 +216,10 @@ plt.xlabel('Age')
 plt.ylabel('Number of people')
 plt.title('Age variety')
 plt.xticks(np.arange(10, 70, 5))
-
 hist_plot = plt.hist(x, bins = 36, ec='black')
+st.pyplot(plot1)
 
-
-# The most popular age to have sex is from 40 to 45 y.o.
+st.subheader("The most popular age to have sex is from 40 to 45 y.o.")
 
 # In[24]:
 
@@ -231,9 +230,10 @@ hist_plot = plt.hist(x, bins=np.arange(5)-0.5, ec='black')
 plt.ylabel('Number of people')
 plt.title('Education variety')
 plt.xticks(size = 8, rotation=0)
+st.pyplot(plot1)
 
 
-# The most popular time to have sex is during Secondary education.
+st.subheader("The most popular time to have sex is during Secondary education.")
 
 # In[25]:
 
@@ -254,7 +254,7 @@ plt.ylabel('Number of people')
 plt.xticks(size = 8, rotation=90)
 
 
-# Most of the people wanted sex mutually, both male and female.
+st.subheader("Most of the people wanted sex mutually, both male and female.")
 
 # In[26]:
 
@@ -272,10 +272,11 @@ plt.legend(loc='upper right')
 plt.title('Kind of sex and eye contact')
 plt.ylabel('Number of people')
 plt.xticks(size = 8, rotation=90)
+st.pyplot(plot1)
 
 
-# It is clear that the most popular sex variant is vaginal sex with straight eye contact.
+st.subheader("It is clear that the most popular sex variant is vaginal sex with straight eye contact.")
 
-# ## Conclusion (+ hyphothesis check)
+st.header("Conclusion (+ hyphothesis check)")
 
-# So, as an overview, my hypothesis was incorrect. Basically, when the mutual agreement occur, it is more likely that both genders will agree to have oral sex. Moreover, eye contact plays a great role in sex, and especially in vaginal sex. The most popular age to have sex is vaginal and the most popular age to hold such an event is 40-45 y.o.
+st.subheader("So, as an overview, my hypothesis was incorrect. Basically, when the mutual agreement occur, it is more likely that both genders will agree to have oral sex. Moreover, eye contact plays a great role in sex, and especially in vaginal sex. The most popular age to have sex is vaginal and the most popular age to hold such an event is 40-45 y.o.")
